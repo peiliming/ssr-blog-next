7e031234d1b4991a53f5c8f4a2b000325a890625
Mingming <peiliming910@gmail.com>
2023-04-09 00:23:35 +0900

    fix:各項目を.mapでループさせる

--- components/AdminNav.tsx
+++ components/AdminNav.tsx
@@ -5,12 +5,17 @@ import Logo from '@/Logo'
 // https://react-icons.github.io/react-icons/
 import { AiOutlineDashboard, AiOutlineContainer, AiOutlineTeam, AiOutlineMail } from 'react-icons/ai'
 import { GrDashboard } from 'react-icons/gr'
-
+import { IconType } from 'react-icons'
 
 interface Props {
+  navItems: {
+    label: string
+    icon: IconType
+    href: string
+  }[]
 }
 
-const AdminNav: FC<Props> = (props): JSX.Element => {
+const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
   return (
     <nav className='h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark'>
       {/* logo */}
@@ -22,30 +27,17 @@ const AdminNav: FC<Props> = (props): JSX.Element => {
       </Link>
       {/* nav items   react iconsを検索 */}
       <div className='space-y-6'>
-        <Link href='/admin'>
-          <div className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.90] transition'>
-            <AiOutlineDashboard size={24} />
-            <span className='ml-2'>ダッシュボード</span>
-          </div>
-        </Link>
-        <Link href='/admin/posts'>
-          <div className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.90] transition'>
-            <AiOutlineContainer size={24} />
-            <span className='ml-2'>ポスト</span>
-          </div>
-        </Link>
-        <Link href='/admin/users'>
-          <div className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.90] transition'>
-            <AiOutlineTeam size={24} />
-            <span className='ml-2'>ユーザー</span>
-          </div>
-        </Link>
-        <Link href='/admin/comments'>
-          <div className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.90] transition'>
-            <AiOutlineMail size={24} />
-            <span className='ml-2'>コメント</span>
-          </div>
-        </Link>
+
+        {navItems.map((item) => {
+          return (
+            <Link key={item.href} href={item.href}>
+            <div className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.90] transition'>
+              <item.icon size={24} />
+              <span className='ml-2'>{item.label}</span>
+            </div>
+          </Link>
+          )
+        })}
       </div>
     </nav>
   )
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,21 @@
 import { NextPage } from 'next'
 import AdminNav from '@/components/AdminNav'
+import { AiOutlineContacts, AiOutlineContainer, AiOutlineDashboard, AiOutlineMail, AiOutlineTeam } from 'react-icons/ai'
 
 interface Props {}
 
+const navItems = [
+  { href: '/admin', icon: AiOutlineDashboard, label:'ダッシュボード' },
+  { href: '/admin/posts', icon: AiOutlineContainer, label:'ポスト' },
+  { href: '/admin/users', icon: AiOutlineTeam, label:'ユーザー' },
+  { href: '/admin/comments', icon: AiOutlineMail, label:'コメント' },
+  { href: '/admin/contact', icon: AiOutlineContacts, label:'コンタクト' },
+]
+
 const Admin: NextPage<Props> = () => {
   return (
-    <div className='dark'>
-      <AdminNav />
+    <div>
+      <AdminNav navItems={navItems} />
     </div>
   )
 }
