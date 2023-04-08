ca7baf26247c30cb2f46c5021de5ad5a15993dc1
Mingming <peiliming910@gmail.com>
2023-04-09 01:06:59 +0900

    add:useState()とuseRef()を使って、トグルボタンの実装

--- components/AdminNav.tsx
+++ components/AdminNav.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState, useRef } from 'react'
 import Link from 'next/link'
 import Logo from '@/Logo'
 // npm install react-icons --save
@@ -6,7 +6,7 @@ import Logo from '@/Logo'
 import { AiOutlineDashboard, AiOutlineContainer, AiOutlineTeam, AiOutlineMail } from 'react-icons/ai'
 import { GrDashboard } from 'react-icons/gr'
 import { IconType } from 'react-icons'
-import { RiMenuFoldFill } from 'react-icons/ri'
+import { RiMenuFoldFill, RiMenuUnfoldFill } from 'react-icons/ri'
 
 interface Props {
   navItems: {
@@ -16,15 +16,37 @@ interface Props {
   }[]
 }
 
+const NAV_OPEN_WIDTH = 'w-60'
+const NAV_CLOSE_WIDTH = 'w-12'
+
 const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
+  const navRef = useRef<HTMLElement>(null)
+  const [visible, setVisible] = useState(true)
+
+  const updateNavState = () => {
+    const { current: currentNav } = navRef  // const currentNav = navRef.current
+    if(!currentNav) return
+
+    const { classList } = currentNav
+    if(visible) {
+      // navを隠す
+      classList.remove(NAV_OPEN_WIDTH)   // if { classList } = currentNavをしなければ、 currentNav.classList.removeになる
+      classList.add(NAV_CLOSE_WIDTH)
+    } else {
+      classList.add(NAV_OPEN_WIDTH)
+      classList.remove(NAV_CLOSE_WIDTH)
+    }
+    setVisible(!visible)
+  }
+
   return (
-    <nav className='h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between'>
+    <nav ref={navRef} className='h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between'>
       <div>
         {/* logo */}
         <Link href='/admin'>
           <div className='flex items-center space-x-2 p-3 mb-10'>
             <Logo className='fill-highlight-light dark:fill-highlight-dark w-5 h-5' />
-            <span className='text-highlight-light dark:fill-highlight-dark text-xl font-semibold'>Admin</span>
+            {visible && <span className='text-highlight-light dark:fill-highlight-dark text-xl font-semibold'>Admin</span>}
           </div>
         </Link>
       
@@ -36,7 +58,7 @@ const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
               <Link key={item.href} href={item.href}>
               <div className='flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.90] transition'>
                 <item.icon size={24} />
-                <span className='ml-2'>{item.label}</span>
+                {visible && <span className='ml-2'>{item.label}</span>}
               </div>
             </Link>
             )
@@ -45,8 +67,8 @@ const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
       </div>
 
       {/* nav toggle button */}
-      <button className='text-highlight-light dark:text-highlight-dark p-3 hover:scale-[0.90] transition self-end'>
-        <RiMenuFoldFill size={25} />
+      <button onClick={updateNavState} className='text-highlight-light dark:text-highlight-dark p-3 hover:scale-[0.90] transition self-end'>
+        {visible ? <RiMenuFoldFill size={25} /> : <RiMenuUnfoldFill size={25} />}
       </button>
     </nav>
   )
