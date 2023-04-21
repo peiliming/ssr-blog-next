046654fcc13e2fe3f43170609afad28d5157d1c5
ppppppsmash <pppppp.work@gmail.com>
2023-04-21 23:24:53 +0900

    add:bubble menuに関するパーツを用意しておく

--- components/editor/Link/EditLink.tsx
+++ components/editor/Link/EditLink.tsx
@@ -1,9 +1,28 @@
 import { FC } from 'react'
+import { BsBoxArrowUpRight, BsPencilSquare } from 'react-icons/bs'
+import { BiUnlink } from 'react-icons/bi'
 
 interface Props {}
 
 const EditLink: FC<Props> = (props): JSX.Element => {
-  return <div>EditLink</div>
+  return (
+    <div>
+      <div className='rounded bg-primay dark:bg-primary-dark text-primary-dark dark:text-primary
+      shadow-secondary-dark'>
+        <button>
+          <BsBoxArrowUpRight />
+        </button>
+
+        <button>
+          <BsPencilSquare />
+        </button>
+
+        <button>
+          <BsBoxArrowUpRight />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default EditLink
