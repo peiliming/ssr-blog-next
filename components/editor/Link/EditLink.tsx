37d8318132e92f776e39f1494b98525e3f8c5c31
ppppppsmash <pppppp.work@gmail.com>
2023-04-21 23:38:46 +0900

    mod:bubble menuからリンクに遷移できるようにした

--- components/editor/Link/EditLink.tsx
+++ components/editor/Link/EditLink.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useCallback } from 'react'
 import { BsBoxArrowUpRight, BsPencilSquare } from 'react-icons/bs'
 import { BiUnlink } from 'react-icons/bi'
 import { BubbleMenu, Editor } from '@tiptap/react'
@@ -8,11 +8,22 @@ interface Props {
 }
 
 const EditLink: FC<Props> = ({editor}): JSX.Element => {
-  const handleOnLinkOpenClick = () => {}
+  const handleOnLinkOpenClick = () => {
+    const { href } = editor.getAttributes('link')
+    if(href) {
+      window.open(href, '_blank')
+    }
+  }
+
   const handleLinkEditClick = () => {}
   const handleUnlinkCLick = () => {}
   return (
-    <BubbleMenu editor={editor}>
+    <BubbleMenu
+      shouldShow={({editor}) =>
+        editor.isActive('link')
+      }
+      editor={editor}
+    >
       <div className='rounded bg-primay dark:bg-primary-dark text-primary-dark dark:text-primary
       shadow-secondary-dark p-3 flex items-center space-x-6 relative z-50 shadow-md'>
         <button onClick={handleOnLinkOpenClick}>
