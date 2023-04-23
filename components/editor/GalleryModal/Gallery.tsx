22d61c2084dfdff983d75040562bd57bd8203868
ppppppsmash <pppppp.work@gmail.com>
2023-04-23 22:48:09 +0900

    add:画像Galleryコンポーネント作成

---
+++ components/editor/GalleryModal/Gallery.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+interface Props {}
+
+const Gallery: FC<Props> = (props): JSX.Element => {
+  return <div>Gallery</div>
+}
+
+export default Gallery
--- components/editor/GalleryModal/index.tsx
+++ components/editor/GalleryModal/index.tsx
@@ -1,5 +1,6 @@
 import ModalContainer, { ModalProps } from '@/components/common/ModalContainer'
 import { FC } from 'react'
+import Gallery from '@/components/editor/GalleryModal/Gallery'
 
 interface Props extends ModalProps {
 
@@ -8,8 +9,15 @@ interface Props extends ModalProps {
 const GalleryModal: FC<Props> = ({visible, onClose}): JSX.Element => {
   return (
     <ModalContainer visible={visible} onClose={onClose}>
-      <div className="bg-black p-20">
-        <button className='bg-white p-3'>クリック</button>
+      <div className="max-w-4xl p-2 bg-primary-dark dark:bg-primary rounded">
+        <div className="flex">
+          {/* gallery */}
+          <div className="basis-[75%]">
+            <Gallery />
+          </div>
+
+          <div className="basis-1/4"></div>
+        </div>
       </div>
     </ModalContainer>  
   )
