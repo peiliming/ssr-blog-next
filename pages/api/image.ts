fc828bae94020f9f2a77ec7615a1de9bebd0b98a
ppppppsmash <pppppp.work@gmail.com>
2023-04-28 22:31:31 +0900

    mod:画像をPOST処理

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -18,7 +18,7 @@ const handler: NextApiHandler = (req, res) => {
 
 const uploadNewImage: NextApiHandler = (req, res) => {
   const form = formidable()
-  form.parse(req, (err, fields, files) => {
+  form.parse(req, async (err, fields, files) => {
     if(err) return res.status(500).json({error: err.message})
 
     const imageFile = files.image as formidable.File
@@ -28,12 +28,27 @@ const uploadNewImage: NextApiHandler = (req, res) => {
     })
 
     res.json({ image: secure_url, url })
-
-
   })
 }
 
-const readAllImages: NextApiHandler = (req, res) => {}
+const readAllImages: NextApiHandler = async (req, res) => {
+  // 参考：https://support.cloudinary.com/hc/en-us/community/posts/360010637199-Upload-image-to-specified-folder-node-js-with-cloudinary-v2
+  try {
+    const { resources } = await cloudinary.api.resources({
+      resorce_type: 'image',
+      type: 'upload',
+      prefix: 'ssr-blogs',
+    })
+  
+    const images = resources.map(({secure_url}: any) => {
+      return {src: secure_url}
+    })
+    res.json({ images })
+  } catch (error: any) {
+    res.status(500).json({error: error.message})
+  }
+  
+}
 
   
 export default handler
