2cb897ddd840e9ac297042732be8f41c3c17333e
ppppppsmash <pppppp.work@gmail.com>
2023-04-30 22:09:36 +0900

    mod:textarea文字数制限処理

--- components/editor/SeoForm.tsx
+++ components/editor/SeoForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, FC } from 'react'
+import { ChangeEventHandler, FC, useState } from 'react'
 import classnames from 'classnames'
 
 interface Props {}
@@ -6,31 +6,51 @@ interface Props {}
 const commonInput = 'w-full bg-transparent outline-none border-2 border-secondary-dark focus:border-primay-dark focus:dark:border-primary rounded transition dark:text-primary p-2'
 
 const SeoForm: FC<Props> = (props): JSX.Element => {
+  const [values, setValues] = useState({meta: '', slug: '', tags: ''})
+
+  const handleChange: ChangeEventHandler<
+  HTMLTextAreaElement | HTMLInputElement
+  > = ({target}) => {
+    let {name, value} = target
+
+    if(name === 'meta') {
+      value = value.slice(0, 150)  // substring(0, 150)
+    }
+    setValues({...values, [name]: value}) // [name]ブラケット記法
+  }
+
+  const {meta, slug, tags} = values
+
   return (
     <div className='space-y-4'>
       <h1
         className='text-primary-dark dark:text-primary text-xl
         font-semibold'>SEO セクション</h1>
       
-      <Input
+      <Input value={slug}
         name='slug'
         placeholder='slug'
         label='Slug:'
+        onChange={handleChange}
       />
 
-      <Input
+      <Input value={tags}
         name='tags'
         placeholder='React, Next JS'
         label='Tags:'
+        onChange={handleChange}
       />
 
       <div className="relative">  
         <textarea
+          name='meta'
+          value={meta}
           className={classnames(commonInput, 'text-xl h-20 resize-none')}
           placeholder='Meta情報, 150文字以内'
+          onChange={handleChange}
         ></textarea>
         <p className='absolute bottom-3 right-3 text-primary-dark
-        dark:text-primary text-sm'>0/150</p>
+        dark:text-primary text-sm'>{meta.length}/150</p>
       </div>
     </div>
   )
