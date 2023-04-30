25f74f24eb2f34ee4180ab84f92ddb0f0cd98ba9
ppppppsmash <pppppp.work@gmail.com>
2023-04-30 21:36:18 +0900

    mod:SEO Inputをコンポーネント化

--- components/editor/SeoForm.tsx
+++ components/editor/SeoForm.tsx
@@ -1,6 +1,9 @@
-import { FC } from 'react'
+import { ChangeEventHandler, FC } from 'react'
+import classnames from 'classnames'
 
 interface Props {}
+
+const commonInput = 'w-full bg-transparent outline-none border-2 border-secondary-dark focus:border-primay-dark focus:dark:border-primary rounded transition dark:text-primary p-2'
 
 const SeoForm: FC<Props> = (props): JSX.Element => {
   return (
@@ -8,23 +11,48 @@ const SeoForm: FC<Props> = (props): JSX.Element => {
       <h1
         className='text-primary-dark dark:text-primary text-xl
         font-semibold'>SEO セクション</h1>
+      
+      <Input
+        name='slug'
+        placeholder='slug'
+        label='Slug:'
+      />
 
-        <label className='block relative'>
-          <span
-            className='absolute top-1/2 -translate-y-1/2 pl-2
-            text-sm font-semibold text-primary-dark dark:text-primary'
-          >
-            Slug:
-          </span>
-          <input
-            type='text'
-            placeholder='slug'
-            className='w-full bg-transparent outline-none border-2 border-secondary-dark
-            focus:border-primay-dark focus:dark:border-primary rounded transition
-            dark:text-primary p-2 italic pl-11'
-          />
-        </label>
+      <Input
+        name='tags'
+        placeholder='React, Next JS'
+        label='Tags:'
+      />
+
+      <textarea
+        className={classnames(commonInput, 'text-xl h-20 resize-none')}
+        placeholder='Meta description'
+      ></textarea>
     </div>
+  )
+}
+
+const Input: FC<{
+  name?: string
+  value?: string
+  placeholder?: string
+  label?: string
+  onChange?: ChangeEventHandler<HTMLInputElement>
+}> = ({name, value, placeholder, label, onChange}) => {
+  return (
+    <label className='block relative'>
+      <span className='absolute top-1/2 -translate-y-1/2 pl-2 text-sm font-semibold text-primary-dark dark:text-primary'>
+        {label}
+      </span>
+      <input
+        type='text'
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        className={classnames(commonInput, 'italic pl-11')}
+        onChange={onChange}
+      />
+    </label>
   )
 }
 
