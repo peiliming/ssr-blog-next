d9c8c2d7d425db5a87fac68967ee36c89ce54a97
ppppppsmash <pppppp.work@gmail.com>
2023-05-01 23:53:03 +0900

    mod:サムネールアップロード処理

--- components/editor/ThumbnailSelector.tsx
+++ components/editor/ThumbnailSelector.tsx
@@ -1,19 +1,35 @@
 import classNames from 'classnames'
-import { FC } from 'react'
+import { ChangeEventHandler, FC, useState } from 'react'
 
-interface Props {}
+interface Props {
+
+}
 
 const commonClass = 'border border-dashed border-secondary-dark flex items-center justify-center rounded cursor-pointer aspect-video'
 
 const ThumbnailSelector: FC<Props> = (props): JSX.Element => {
+  const [selectedThumbnail, setSelectedThumbnail] = useState('')
+  const handleChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    const { files } = target
+    if(!files) return
+
+    const file = files[0]
+    setSelectedThumbnail(URL.createObjectURL(file))
+  }
   return (
     <div className='w-32'>
-      <input type='file' accept='image/*'
-      id='thumbnail'
-      hidden
+      <input
+        type='file'
+        accept='image/*'
+        hidden
+        id='thumbnail'
+        onChange={handleChange}
       />
       <label htmlFor='thumbnail'>
-        <PosterUI label='サムネール' />
+      {selectedThumbnail ? 
+        (<img src={selectedThumbnail} alt='' />) : 
+        (<PosterUI label='サムネール' />
+      )}
       </label>
     </div>
   )
