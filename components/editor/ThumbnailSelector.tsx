42cdc54db3c9e5a2d68752b09f75c480319825a0
ppppppsmash <pppppp.work@gmail.com>
2023-05-01 23:45:06 +0900

    mod:サムネールコンポーネント

--- components/editor/ThumbnailSelector.tsx
+++ components/editor/ThumbnailSelector.tsx
@@ -1,11 +1,31 @@
+import classNames from 'classnames'
 import { FC } from 'react'
 
 interface Props {}
 
+const commonClass = 'border border-dashed border-secondary-dark flex items-center justify-center rounded cursor-pointer aspect-video'
+
 const ThumbnailSelector: FC<Props> = (props): JSX.Element => {
   return (
-    <div>
-      
+    <div className='w-32'>
+      <input type='file' accept='image/*'
+      id='thumbnail'
+      hidden
+      />
+      <label htmlFor='thumbnail'>
+        <PosterUI label='サムネール' />
+      </label>
+    </div>
+  )
+}
+
+const PosterUI: FC<{ label: string, className?: string}> = ({
+  label,
+  className
+}) => {
+  return (
+    <div className={classNames(commonClass, className)}>
+      <span>{label}</span>
     </div>
   )
 }
