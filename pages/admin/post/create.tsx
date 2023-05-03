8516a4a43e5b0f6c5f08addb026408629ae6a028
ppppppsmash <pppppp.work@gmail.com>
2023-05-03 22:52:27 +0900

    add:Headコンポーネント作成&ページごと<title>設定

--- pages/admin/post/create.tsx
+++ pages/admin/post/create.tsx
@@ -1,24 +1,27 @@
 import Editor from '@/components/editor'
+import AdminLayout from '@/components/layout/AdminLayout'
 import { NextPage } from 'next'
 
 interface Props {}
 
 const Create: NextPage<Props> = () => {
   return (
-    <div className='max-w-4xl mx-auto'>
-      <Editor onSubmit={(post) => {
-          console.log(post)
-        }}
-        initialValue={{
-          title: '',
-          meta: '',
-          content: '',
-          slug: '',
-          tags: '',
-          thumbnail: ''
-        }}
-      />
-    </div>
+    <AdminLayout title='新しいポスト'>
+      <div className='max-w-4xl mx-auto'>
+        <Editor onSubmit={(post) => {
+            console.log(post)
+          }}
+          initialValue={{
+            title: '',
+            meta: '',
+            content: '',
+            slug: '',
+            tags: '',
+            thumbnail: ''
+          }}
+        />
+      </div>
+    </AdminLayout>
   )
 }
 
