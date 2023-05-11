92fb28910cf8b58c1f139d92c496b24560b66192
ppppppsmash <pppppp.work@gmail.com>
2023-05-11 23:11:10 +0900

    mod:Postスキーマを完成

--- app/models/Post.ts
+++ app/models/Post.ts
@@ -1,39 +1,46 @@
 // title, content, slug, tags, thumbnail, meta, author, date
-import { Schema } from 'mongoose'
+import { Schema, model } from 'mongoose'
 
 ('タイトル')
-const PostSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    meta: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tags: {
+      type: [String],
+    },
+    thumbnail: {
+      type: Object,
+      url: String,
+      public_id: String
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
   },
-  slug: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true
-  },
-  content: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  meta: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  tags: {
-    type: [String],
-  },
-  thumbnail: {
-    type: Object,
-    url: String,
-    public_id: String
-  },
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
+  {
+    timestamps: true
   }
-})
+)
+
+const Post = model('Post', PostSchema)
