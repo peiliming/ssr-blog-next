8ebe088f3f53efe4169e116cc2588e6758b8578a
ppppppsmash <pppppp.work@gmail.com>
2023-05-12 21:50:37 +0900

    mod:postmanで/api/postsでBody raw JSON {}でPOSTのバリデーションテスト

--- app/pages/api/posts/index.ts
+++ app/pages/api/posts/index.ts
@@ -1,5 +1,6 @@
 import { NextApiHandler } from "next";
 import dbConnect from "@/lib/dbConnect";
+import Joi from 'joi'
 
 const handler: NextApiHandler = async (req, res) => {
   const { method } = req
@@ -8,7 +9,25 @@ const handler: NextApiHandler = async (req, res) => {
       await dbConnect()
       res.json({ ok: true })
     }
+    case 'POST': return createNewPost(req, res)
   }
+}
+
+const createNewPost: NextApiHandler = (req, res) => {
+  const {body} = req
+
+  const schema = Joi.object().keys({
+    title: Joi.string().required()
+  })
+
+  const {error} = schema.validate(body)
+  if(error) {
+    const errorMessage = error.details[0].message
+    return res.status(400).json({ error: errorMessage })
+  }
+
+  res.json({ ok: true })
+  
 }
 
 export default handler
