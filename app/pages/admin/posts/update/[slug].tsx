a2c51136d2e7ec706dbc3cf4613fd6fa480b574f
ppppppsmash <pppppp.work@gmail.com>
2023-05-22 21:56:56 +0900

    mod: slugページ反映処理

--- app/pages/admin/posts/update/[slug].tsx
+++ app/pages/admin/posts/update/[slug].tsx
@@ -1,15 +1,15 @@
 import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import { FinalPost } from '@/components/editor'
+import dbConnect from '@/lib/dbConnect'
+import Post from '@/models/Post'
+import AdminLayout from '@/components/layout/AdminLayout'
+import Editor from '@/components/editor'
 
 interface PostResponse extends FinalPost {
   id: string
 }
 
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>
-
-const Update: NextPage<Props> = ({ post }) => {
-  return (<div>Updateページ</div>)
-}
 
 interface ServerSideResponse {
   post: PostResponse
@@ -18,12 +18,35 @@ interface ServerSideResponse {
 export const getServerSideProps: GetServerSideProps<
   ServerSideResponse
 > = async (context) => {
-  console.log(context)
+  const slug = context.query.slug as string
+
+  await dbConnect()
+  const post = await Post.findOne({ slug })
+  if(!post) return { notFound: true }
+
+  const {_id, meta, title, content, thumbnail, tags} = post
+
   return {
     props: {
-      post: {}
+      post: {
+        id: _id.toString(),
+        title,
+        content,
+        tags: tags.join(', '),
+        thumbnail: thumbnail?.url || '',
+        slug
+      }
     }
   }
+}
+
+const Update: NextPage<Props> = ({ post }) => {
+  return (
+    <AdminLayout>
+      {/* /admin/posts/update/testtest (slug) */}
+      <Editor initialValue={post} onSubmit={() => {}} btnTitle='更新' />
+    </AdminLayout>
+  )
 }
 
 export default Update
