a89572f0660683a8d5eaaf5c3c100a3286118f3b
ppppppsmash <pppppp.work@gmail.com>
2023-05-22 22:12:00 +0900

    mod: handSubmit処理を汎用フォルダーに調整

--- app/pages/admin/posts/update/[slug].tsx
+++ app/pages/admin/posts/update/[slug].tsx
@@ -4,6 +4,8 @@ import dbConnect from '@/lib/dbConnect'
 import Post from '@/models/Post'
 import AdminLayout from '@/components/layout/AdminLayout'
 import Editor from '@/components/editor'
+import { generateFormData } from '@/utils/helper'
+import axios from 'axios'
 
 interface PostResponse extends FinalPost {
   id: string
@@ -18,33 +20,48 @@ interface ServerSideResponse {
 export const getServerSideProps: GetServerSideProps<
   ServerSideResponse
 > = async (context) => {
-  const slug = context.query.slug as string
+  try {
+    const slug = context.query.slug as string
 
-  await dbConnect()
-  const post = await Post.findOne({ slug })
-  if(!post) return { notFound: true }
+    await dbConnect()
+    const post = await Post.findOne({ slug })
+    if(!post) return { notFound: true }
 
-  const {_id, meta, title, content, thumbnail, tags} = post
+    const {_id, meta, title, content, thumbnail, tags} = post
 
-  return {
-    props: {
-      post: {
-        id: _id.toString(),
-        title,
-        content,
-        tags: tags.join(', '),
-        thumbnail: thumbnail?.url || '',
-        slug
+    return {
+      props: {
+        post: {
+          id: _id.toString(),
+          title,
+          content,
+          tags: tags.join(', '),
+          thumbnail: thumbnail?.url || '',
+          slug
+        }
       }
     }
+  } catch (error) {
+    return { notFound: true }
   }
 }
 
 const Update: NextPage<Props> = ({ post }) => {
+  const handleSubmit = async (post: FinalPost) => {
+    try {
+      // FormData
+      const formData = generateFormData(post)
+      // submit post
+      const {data} = await axios.patch('/api/posts', formData)
+      console.log(data)
+    } catch (error: any) {
+      console.log(error.response.data)
+    }
+  }
   return (
     <AdminLayout>
       {/* /admin/posts/update/testtest (slug) */}
-      <Editor initialValue={post} onSubmit={() => {}} btnTitle='更新' />
+      <Editor initialValue={post} onSubmit={handleSubmit} btnTitle='更新' />
     </AdminLayout>
   )
 }
