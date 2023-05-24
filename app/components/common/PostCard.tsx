269bb7d8d86036133458222cd4fe7a590d33e795
ppppppsmash <pppppp.work@gmail.com>
2023-05-24 22:13:32 +0900

    mod: ポストカードスタイル続き

--- app/components/common/PostCard.tsx
+++ app/components/common/PostCard.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react'
-import { PostDetail } from '@/utils/types'
 import Image from 'next/image'
+import { PostDetail } from '@/utils/types'
+import dateformat from 'dateformat'
 
 interface Props {
   post: PostDetail
+}
+
+const trimText = (text: string, trimBy: number) => {
+  if(text.length <= trimBy) {
+    return text
+  }
+  return text.substring(0, trimBy).trim() + '...'
 }
 
 const PostCard: FC<Props> = ({post}): JSX.Element => {
@@ -26,11 +34,25 @@ const PostCard: FC<Props> = ({post}): JSX.Element => {
       <div className='p-2 flex-1 flex flex-col'>
         <div className='flex items-center justify-between text-sm
         text-primary-dark dark:text-primary'>
-          {tags.map((t, index) => (
-            <span key={t + index}>#{t}</span>
-          ))}
+          <div className='flex items-center space-x-1'>
+            {tags.map((t, index) => (
+              <span key={t + index}>#{t}</span>
+            ))}
+          </div>
+          <span>{dateformat(createdAt, 'd-mmm-yyyy')}</span>
         </div>
-        <span>{createdAt}</span>
+
+        <h1 className='font-semibold text-primary-dark dark:text-primary'>
+          {trimText(title, 50)}
+        </h1>
+        <p className='text-secondary-dark'>
+          {trimText(meta, 70)}
+        </p>
+        <div className='flex justify-end items-center h-8 mt-auto
+        space-x-4 text-primary-dark dark:text-primary'>
+          <button className='hover:underline'>編集</button>
+          <button className='hover:underline'>削除</button>
+        </div>
       </div>
     </div>
   )
