2358113efba0bbbe4af58fe60f9be7e8c60f1615
ppppppsmash <pppppp.work@gmail.com>
2023-05-24 22:48:20 +0900

    mod: ポスト時のgetServerSideProps

--- app/pages/admin/posts/index.tsx
+++ app/pages/admin/posts/index.tsx
@@ -1,39 +1,21 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import AdminLayout from '@/components/layout/AdminLayout'
 import { useState } from 'react'
 import PostCard from '@/components/common/PostCard'
 import Link from 'next/link'
+import { PostDetail } from '@/utils/types'
 
-interface Props {}
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>
 
-const posts = [
-  {
-    title: 'test test test',
-    slug: 'test',
-    meta: 'test',
-    tags: ['test'],
-    thumbnail: 'https://images.unsplash.com/photo-1664784805210-9fa665e2b7e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-    createdAt: 'Tue Oct 11 2022 14:58:49'
-  },
-  {
-    title: 'test test test',
-    slug: 'test',
-    meta: 'test',
-    tags: ['test'],
-    thumbnail: 'https://images.unsplash.com/photo-1664784805210-9fa665e2b7e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-    createdAt: 'Tue Oct 11 2022 14:58:49'
-  },
-  {
-    title: 'test test test',
-    slug: 'test',
-    meta: 'test',
-    tags: ['test'],
-    thumbnail: 'https://images.unsplash.com/photo-1664784805210-9fa665e2b7e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-    createdAt: 'Tue Oct 11 2022 14:58:49'
-  }
-]
+interface ServerSideResponse {
+  posts: PostDetail[]
+}
 
-const Posts: NextPage<Props> = () => {
+export const getServerSideProps: GetServerSideProps<ServerSideResponse> = async () => {
+
+}
+
+const Posts: NextPage<Props> = ({posts}) => {
   const [postsToRender, setPostsToRender] = useState(posts)
   return (<div>
     <AdminLayout>
@@ -41,7 +23,7 @@ const Posts: NextPage<Props> = () => {
         <div className="grid grid-cols-3 gap-4">
           {postsToRender.map(post => (
             <Link key={post.slug} href={'/' + post.slug}>
-              <PostCard key={post.slug} post={post} busy />
+              <PostCard key={post.slug} post={post} />
             </Link>
           ))}
         </div>
