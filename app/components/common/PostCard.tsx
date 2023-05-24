347a36c9eba97b99e1b150b8ceda5926eb1c8207
ppppppsmash <pppppp.work@gmail.com>
2023-05-24 22:29:48 +0900

    add: next/link周り

--- app/components/common/PostCard.tsx
+++ app/components/common/PostCard.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import Image from 'next/image'
 import { PostDetail } from '@/utils/types'
 import dateformat from 'dateformat'
+import Link from 'next/link'
 
 interface Props {
   post: PostDetail
@@ -32,25 +33,29 @@ const PostCard: FC<Props> = ({post}): JSX.Element => {
           )}
       </div>
       <div className='p-2 flex-1 flex flex-col'>
-        <div className='flex items-center justify-between text-sm
-        text-primary-dark dark:text-primary'>
-          <div className='flex items-center space-x-1'>
-            {tags.map((t, index) => (
-              <span key={t + index}>#{t}</span>
-            ))}
+        <Link href={'/' + slug}>
+          <div className='flex items-center justify-between text-sm
+          text-primary-dark dark:text-primary'>
+            <div className='flex items-center space-x-1'>
+              {tags.map((t, index) => (
+                <span key={t + index}>#{t}</span>
+              ))}
+            </div>
+            <span>{dateformat(createdAt, 'd-mmm-yyyy')}</span>
           </div>
-          <span>{dateformat(createdAt, 'd-mmm-yyyy')}</span>
-        </div>
 
-        <h1 className='font-semibold text-primary-dark dark:text-primary'>
-          {trimText(title, 50)}
-        </h1>
-        <p className='text-secondary-dark'>
-          {trimText(meta, 70)}
-        </p>
+          <h1 className='font-semibold text-primary-dark dark:text-primary'>
+            {trimText(title, 50)}
+          </h1>
+          <p className='text-secondary-dark'>
+            {trimText(meta, 70)}
+          </p>
+        </Link>
         <div className='flex justify-end items-center h-8 mt-auto
         space-x-4 text-primary-dark dark:text-primary'>
-          <button className='hover:underline'>編集</button>
+          <Link className='hover:underline' href={'/admin/posts/update/' + slug}>
+            編集
+          </Link>
           <button className='hover:underline'>削除</button>
         </div>
       </div>
--- app/pages/admin/posts/index.tsx
+++ app/pages/admin/posts/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next'
 import AdminLayout from '@/components/layout/AdminLayout'
 import { useState } from 'react'
 import PostCard from '@/components/common/PostCard'
+import Link from 'next/link'
 
 interface Props {}
 
@@ -39,7 +40,9 @@ const Posts: NextPage<Props> = () => {
       <div className="max-w-4xl mx-auto p-3">
         <div className="grid grid-cols-3 gap-4">
           {postsToRender.map(post => (
-            <PostCard post={post} />
+            <Link key={post.slug} href={'/' + post.slug}>
+              <PostCard key={post.slug} post={post} />
+            </Link>
           ))}
         </div>
       </div>
