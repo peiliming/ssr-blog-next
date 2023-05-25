f454d6bdb3ab51bee69f6f1555092ecd8d0d0c65
ppppppsmash <pppppp.work@gmail.com>
2023-05-25 21:59:52 +0900

    mod: 無限スクロールコンポーネント処理改修

--- app/pages/admin/posts/index.tsx
+++ app/pages/admin/posts/index.tsx
@@ -1,10 +1,10 @@
 import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import AdminLayout from '@/components/layout/AdminLayout'
 import { useState } from 'react'
-import PostCard from '@/components/common/PostCard'
-import Link from 'next/link'
 import { PostDetail } from '@/utils/types'
 import { formatPosts, readPostsFromDb } from '@/lib/utils'
+import axios from 'axios'
+import InfiniteScrollPosts from '@/components/common/InfiniteScrollPosts'
 
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>
 
@@ -32,17 +32,32 @@ export const getServerSideProps: GetServerSideProps<ServerSideResponse> = async 
 
 const Posts: NextPage<Props> = ({posts}) => {
   const [postsToRender, setPostsToRender] = useState(posts)
+  const [hasMorePosts, setHasMorePosts] = useState(true)
+
+  const fetchMorePosts = async () => {
+    try {
+      pageNo++
+      const { data } = await axios(`/api/posts?limit=${limit}&pageNo=${pageNo}`)
+      if(data.posts.length < limit) {
+        setPostsToRender([...postsToRender, ...data.posts])
+        setHasMorePosts(false)
+      } else {
+        setPostsToRender([...postsToRender, ...data.posts])
+      }
+    } catch (error) {
+      setHasMorePosts(false)
+      console.log(error)
+    }
+  }
   return (<div>
     <AdminLayout>
-      <div className="max-w-4xl mx-auto p-3">
-        <div className="grid grid-cols-3 gap-4">
-          {postsToRender.map(post => (
-            <Link key={post.slug} href={'/admin/posts/update/' + post.slug}>
-              <PostCard key={post.slug} post={post} />
-            </Link>
-          ))}
-        </div>
-      </div>
+      <InfiniteScrollPosts
+        hasMore={hasMorePosts}
+        next={fetchMorePosts}
+        dataLength={postsToRender.length}
+        posts={postsToRender}
+        showControls
+      />
     </AdminLayout>
   </div>)
 }
